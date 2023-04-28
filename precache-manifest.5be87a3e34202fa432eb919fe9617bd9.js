fe1b3b53c26e1ab0a1e8e4dee492e2be3a24bf4f
Moses <hiswordmakehim@gmail.com>
2023-04-28 12:22:05 +0100

    adding project

---
+++ precache-manifest.5be87a3e34202fa432eb919fe9617bd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09efe52a8e61db30684e2d2febf3f0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d838c0bb522abe6c5877",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "239576bf5c4a90a8c619",
+    "url": "/static/css/main.6409b829.chunk.css"
+  },
+  {
+    "revision": "d838c0bb522abe6c5877",
+    "url": "/static/js/2.526585eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.526585eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239576bf5c4a90a8c619",
+    "url": "/static/js/main.a351d15d.chunk.js"
+  },
+  {
+    "revision": "df3626599cd0ef421114",
+    "url": "/static/js/runtime-main.75482288.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
